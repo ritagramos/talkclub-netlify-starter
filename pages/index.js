aa61e53d105cdb7c6bc625d69e743076ee3a0370
ritagramos <99331719+ritagramos@users.noreply.github.com>
2022-02-09 15:51:45 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
       </main>
 
       <Footer />
+    <!-- Start of Talkdesk Code -->    <script>      var webchat;      ((window, document, node, props, configs) => {        if (window.TalkdeskChatSDK) {          console.error("TalkdeskChatSDK already included");          return;        }        var divContainer = document.createElement("div");        divContainer.id = node;        document.body.appendChild(divContainer);        var currentDate = new Date().getTime();        var src = `https://talkdeskchatsdk.svc.talkdeskstg.com/talkdeskchatsdk.js?t=` + currentDate;        var script = document.createElement("script");        var firstScriptTag = document.getElementsByTagName("script")[0];        script.type = "text/javascript";        script.charset = "UTF-8";        script.id = "tdwebchatscript";        script.src = src;        script.async = true;        firstScriptTag.parentNode.insertBefore(script, firstScriptTag);        script.onload = () => {          webchat = TalkdeskChatSDK(node, props);          webchat.init(configs);        };      })(        window,        document,        "tdWebchat",        { flowId: "002216a185bc45da9a4eb33a3922ebff", accountId: "" },        { enableEmoji: true, enableUserInput: true }      );    </script><!-- End of Talkdesk Code -->
     </div>
   )
 }
